aa450326ed0ffed0772bb543218dc1e10e59776e
Fedor <qwertyfredya@gmail.com>
2020-11-02 07:40:31 +0700

    add

--- src/Components/Api/Api.js
+++ src/Components/Api/Api.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://praktikum.tk/cohort11',
+  baseURL: 'https://nomoreparties.co/cohort11',
   headers: {
     authorization: '4a26d64f-13d9-4fd9-978e-f9c29a1c49d5',
     'Content-Type': 'application/json'
